31ba98abcca170670127a8cdfef7fe9f483f52cc
Pedro Valentim <pedro@vlt.im>
2017-03-24 17:50:02 +0000

    Update text

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const lolcatjs = require('lolcatjs')
 const cowsay = require('cowsay')
 const text = `
-Thanks for installing create-vitange-app.
-The true connoisseur doesn't use NPM.
+Thanks for installing create-vitange-app
+The true connoisseur doesn't use NPM
 `
 
 lolcatjs.options.seed = Math.round(Math.random() * 1000)
